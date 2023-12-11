ef236dd15372a7fa5d8311a18316be4d05502de3
Shiham123 <s36651786@gmail.com>
2023-12-11 09:49:39 +0600

    navbar

--- src/Components/NavbarSection.jsx
+++ src/Components/NavbarSection.jsx
@@ -1,27 +1,43 @@
+import { NavLink } from 'react-router-dom';
+import Logo from '../assets/_06cc4017-b38b-4b61-a4a2-2871c9b6d694.jpg';
+
+const data = [
+  { label: 'Home', to: '/' },
+  { label: 'Contact', to: '/contact' },
+];
+
 const NavbarSection = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-colorOne p-8">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <div className="flex justify-center items-center gap-4">
+          <img
+            src={Logo}
+            width={70}
+            height={70}
+            className="rounded-full"
+            alt=""
+          />
+          <h1 className="font-caveat text-5xl">Shiham</h1>
+        </div>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Link</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 rounded-t-none">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
+          <div className="flex gap-8">
+            {data.map((item, index) => {
+              const { label, to } = item;
+              return (
+                <div key={index}>
+                  <NavLink
+                    to={to}
+                    className={({ isActive }) => (isActive ? '' : '')}
+                  >
+                    <li>{label}</li>
+                  </NavLink>
+                </div>
+              );
+            })}
+          </div>
         </ul>
       </div>
     </div>
